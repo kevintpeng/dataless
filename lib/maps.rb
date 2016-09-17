c60044433dad9f9028cd93775c44ddb5f71a78d4
kevintpeng <kpeng97@gmail.com>
2016-09-17 11:56:45 -0400

    fixes

--- lib/maps.rb
+++ lib/maps.rb
@@ -3,11 +3,11 @@ require 'rails-html-sanitizer'
 require 'json'
 
 module Maps
-  def directions(origin, destination)
+  def self.directions(origin, destination)
     origin.gsub!(" ", "+").gsub!("_", "+")
     destination.gsub!(" ", "+").gsub!("_", "+")
     full_sanitizer = Rails::HTML::Sanitizer
-    output = []; 
+    output = [];
     # does the thing
     JSON.load(RestClient.get("https://maps.googleapis.com/maps/api/directions/json?origin=#{origin}&destination=#{destination}&key=#{ENV['MAPS_API_KEY']}").body)["routes"][0]["legs"].each {|e| e["steps"].each_with_index
       {|t,i| output << "[#{i}/#{e["steps"].size}] In #{t['distance']['text']}, #{full_sanitizer.sanitize(t['html_instructions'])}"} }
