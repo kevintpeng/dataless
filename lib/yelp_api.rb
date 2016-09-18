45b33ae8d1a2ca94b45f6ce7c1b35713bcb043cf
kevintpeng <kpeng97@gmail.com>
2016-09-17 20:41:21 -0400

    fixed client for yelp

--- lib/yelp_api.rb
+++ lib/yelp_api.rb
@@ -2,14 +2,15 @@ require 'yelp'
 
 module YelpAPI
   def self.connect(search_term, location)
-    client = Yelp::Client.new({ consumer_key: ENV['YELP_CONSUMER_KEY'],
-                            consumer_secret: ENV['YELP_CONSUMER_SECRET'],
-                            token: ENV['YELP_TOKEN'],
-                            token_secret: ENV['YELP_TOKEN_SECRET']
-                          })
+    Yelp.client.configure do |config|
+      config.consumer_key = ENV['YELP_CONSUMER_KEY']
+      config.consumer_secret = ENV['YELP_CONSUMER_SECRET']
+      config.token = ENV['YELP_TOKEN']
+      config.token_secret = ENV['YELP_TOKEN_SECRET']
+    end
     options = {}
     options[:term] = search_term if search_term
-    results = client.search(location, options)
+    results = Yelp.client.search(location, options)
     all_results = []
     results.businesses.each { |business| all_results << "#{business.rating}: #{business.name} is at #{business.location.address.join(", ")}"}
     return all_results
